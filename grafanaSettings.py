2aec599c9c3a61890a4975fbfc33bd85a989952d
vagrant <mtb.vang@gmail.com>
2018-03-25 21:11:39 +0000

    Added twitter keys

--- grafanaSettings.py
+++ grafanaSettings.py
@@ -1,7 +1,7 @@
 import os
 
-grafana_url = os.getenv('GRAFANA_URL', 'http://localhost:3000')
-token = os.getenv('GRAFANA_TOKEN', '')
+grafana_url = os.getenv('GRAFANA_URL', 'http://grafana-kaidu.192.168.162.3.nip.io')
+token = os.getenv('GRAFANA_TOKEN', 'eyJrIjoiekw1cktzanZTWDBXclltTjZBOUJ3OEx2Y2lWRDFQT2EiLCJuIjoiZGV2ZWxvcGVyIiwiaWQiOjF9')
 http_get_headers = {'Authorization': 'Bearer ' + token}
 http_post_headers = {'Authorization': 'Bearer ' + token, 'Content-Type': 'application/json'}
 
